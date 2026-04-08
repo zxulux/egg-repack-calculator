3713165a50f8594beeecd5164d4d88ca9f0cbe98
zxulux <zxul666@gmail.com>
2026-04-07 21:34:00 -0700

    Update db.tsx

--- helpers/db.tsx
+++ helpers/db.tsx
@@ -6,7 +6,23 @@ import postgres from 'postgres'
 export const db = new Kysely<DB>({
 plugins: [new CamelCasePlugin()],
 dialect: new PostgresJSDialect({
-postgres: postgres(process.env.FLOOT_DATABASE_URL, {
+const connectionString =
+  process.env.FLOOT_DATABASE_URL || process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("No database URL provided");
+}
+
+export const db = new Kysely<DB>({
+  plugins: [new CamelCasePlugin()],
+  dialect: new PostgresJSDialect({
+    postgres: postgres(connectionString, {
+      prepare: false,
+      idle_timeout: 10,
+      max: 3,
+    }),
+  }),
+});
 prepare: false,
 idle_timeout: 10,
 max: 3,
